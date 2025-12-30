4b0d7b770facb6f8868ddc335a1bfedeacb3f06a
rogercarbajallopez-dot <roger.carbajal.lopez@gmail.com>
2025-12-29 20:45:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ if API_KEY_GOOGLE:
 else:
     print("❌ ALERTA: No se encontró la API KEY de Google.")
 
-MODELO_IA = "gemini-2.5-flash" 
+MODELO_IA = "gemini-2.0-flash" 
 
 # Conexión Supabase
 supabase: Client = None
@@ -416,3 +416,4 @@ async def webhook_whatsapp(request: Request):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
+
